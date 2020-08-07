68cf1662ccef78048b68e68b601abba248b1da3f
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-07 16:36:43 -0300

    Criado o layout inicial da calculadora

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>Calculadora</h1>
     <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
