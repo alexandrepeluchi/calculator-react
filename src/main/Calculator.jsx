cb84b6c6c743e66abf4651141c6a1e0443a62fc3
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-07 16:28:50 -0300

    Criado novo projeto react

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            // para referenciar uma classe no jsx usamos className
+            <div className="calculator">
+
+            </div>
+        )
+    }
+}
