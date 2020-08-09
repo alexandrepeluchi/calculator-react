6067a765cb3f0a0bb89249dfcf1c8cfe3b564c36
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-08 21:08:29 -0300

    Alterando o nome do h1 da tela da Calculadora

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Calculadora</h1>
+    <h1>Simple Calculator</h1>
     <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
